5d5972cb0d141aa696efa0aef645b1c810ceb7ab
Hatice Boyraz <hboyraz@turksat.com.tr>
2024-06-05 10:15:23 +0300

    Initial Commit

---
+++ Tests/FrameworkPackageTests/FrameworkPackageTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import FrameworkPackage
+
+final class FrameworkPackageTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}

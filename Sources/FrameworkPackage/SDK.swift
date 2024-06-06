f1e45189a71511ddbac5bec7f1ca5f22d55033a3
Hatice Boyraz <hboyraz@turksat.com.tr>
2024-06-06 14:26:23 +0300

    update SDK

--- Sources/FrameworkPackage/SDK.swift
+++ Sources/FrameworkPackage/SDK.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 public struct SDK {
-    public static func doSomeWork() {
+    public static func doSomeWork(name: String) -> String {
         print("Doing some work...")
+        let message = "Hello, \(name)!"
+        Logger.shared.log(message, level: .info)
+        return message
     }
 }

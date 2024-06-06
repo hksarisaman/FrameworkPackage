b783c9f9dd19e5b8005456c8cef81a21b718fd94
Hatice Boyraz <hboyraz@turksat.com.tr>
2024-06-06 13:46:31 +0300

    added logger page

--- Sources/FrameworkPackage/Logger.swift
+++ Sources/FrameworkPackage/Logger.swift
@@ -1,8 +1,19 @@
-//
-//  File.swift
-//  
-//
-//  Created by Hatice Boyraz on 6.06.2024.
-//
-
 import Foundation
+
+public class Logger {
+    public static let shared = Logger()
+
+    private init() {}
+
+    public func log(_ message: String, level: LogLevel = .info) {
+        let formattedMessage = "\(Date()) [\(level.rawValue.uppercased())]: \(message)"
+        print(formattedMessage)
+    }
+
+    public enum LogLevel: String {
+        case debug
+        case info
+        case warning
+        case error
+    }
+}
